83f41e57185506485e0010743a685136b1ccf63d
ari23 <46127147+ari23ant@users.noreply.github.com>
2020-02-14 14:23:23 +0900

    引数機能追加とリファクタリングなど
    - __version__, __status__追加
    - 引数機能追加
    - リファクタリング

--- template.py
+++ template.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
+"""
 Pythonスクリプトテンプレート
 - 2019/10/07| テンプレート作成
 - 2020/01/14| IPythonおまじない修正とログ追加
-'''
-__author__ = 'ari23(Twitter: ai23ant)'
-__date__ = 'yyyy/mm/dd'
+- 2020/01/14| versionとstatus追加、引数機能、リファクタリング
+"""
+__author__  = 'ari23(Twitter: ai23ant)'
+__version__ = '0.0.0'
+__date__    = '2020/02/14'
+__status__  = 'Development'
 
 
 import time
@@ -30,48 +33,55 @@ if __name__ == '__main__':
         get_ipython().magic('reset -sf')
         print('IPython reset command')
 
-    #---------- Program Start ----------#
+    # ---------- Program Start ---------- #
     start_time = time.perf_counter()
     print('---------- Start ----------')
 
     '''
-    #--- ログ用意 ---#
+    # --- ログ用意 --- #
     # ロガー
     logger = logging.getLogger('ControlADIWatch')
     logger.setLevel(logging.DEBUG)
-
     # フォーマット
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
     # コンソール出力用
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(formatter)
-
     # ファイル出力用
     file_handler = logging.FileHandler('nk_python.log', 'a')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
-
     # ロガーに追加
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
-
     logger.propagate = False
-
     '''
  
-    #--- Template ---#
+    # --- Template --- #
     proc = Template()
     proc.Process()
 
     '''
-    #--- 終了処理 ---#
+    # --- Get Argument --- #
+    args = sys.argv  # list
+
+    # --- Main Process --- #
+    if len(args) == 1:
+        proc = Template('')
+    else:  # argsの大きさが0になることはない
+        proc = Template(args[1])
+
+    proc.Process()
+    '''
+
+    '''
+    # --- 終了処理 --- #
     # 重複出力を避けるおまじない
     del logging.Logger.manager.loggerDict[logger.name]
     '''
 
-    #---------- Program End ----------#
+    # ---------- Program End ---------- #
     end_time = time.perf_counter()
     execution_time = end_time - start_time
     print('Execution Time: ' + str(execution_time) + 's')
