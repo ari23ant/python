3ada7273e8ba229f5f94ac5ddb8b8ed83699b294
ari23 <46127147+ari23ant@users.noreply.github.com>
2020-06-02 16:38:45 +0900

    plt.figure()があるとき削除する機能追加

--- template.py
+++ template.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Pythonスクリプトテンプレート
-
 更新履歴
 - 2019/10/07| テンプレート作成
 - 2020/01/14| IPythonおまじない修正とログ追加
 - 2020/02/14| versionとstatus追加、引数機能、リファクタリング
 - 2020/03/06| self.string→self.msgなどリファクタリング
 - 2020/04/05| リファクタリング
+- 2020/06/02| plt.figure()があったら削除する機能追加
 """
 __author__  = 'ari23(Twitter: @ari23ant)'
-__version__ = '0.0.5'
-__date__    = '2020/04/05'
+__version__ = '0.0.6'
+__date__    = '2020/06/02'
 __status__  = 'Development'
 
 #import os
@@ -28,14 +28,21 @@ class Template:
 
     def Process(self):
         print(self.msg)
-        logger.debug(self.msg)
+        #logger.debug(self.msg)
 
 if __name__ == '__main__':
     # IPython使用時のおまじない
     if get_ipython().__class__.__name__ == 'TerminalInteractiveShell':
+        # plt.fiugre()があったら削除
+        import sys
+        if 'matplotlib.pyplot' in sys.modules:
+            #import matplotlib.pyplot
+            #matplotlib.pyplot.close('all')
+            plt.close('all')
+            print('close all figures')
+        # IPython Resetコマンド
         get_ipython().magic('reset -sf')
         print('IPython reset command')
-
     # ---------- Program Start ---------- #
     start_time = time.perf_counter()
     print('---------- Start ----------')
