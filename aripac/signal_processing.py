f9eed79a4e8d5edd88e676dbff00088f6d34d1f5
ari23 <46127147+ari23ant@users.noreply.github.com>
2023-02-24 15:21:30 +0900

    fft() -> plotfft(), fvtool() -> plotfreqz(), etc.

--- aripac/signal_processing.py
+++ aripac/signal_processing.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-'''信号処理
+"""Signal processing plot functions.
+
 周波数解析や周波数応答の関数
-'''
+"""
 __author__  = 'ari23 (Twitter: ari23ant)'
-__version__ = '0.0.1'
-__date__    = '2020/03/06'
-__status__  = 'Development'
+__version__ = '0.2.7'
+__date__ = '2023/02/24'
+__status__ = 'Development'
 
-import time
-import pandas as pd
 import numpy as np
-from scipy import signal
-from scipy import fftpack
+from scipy import signal, fft
 import matplotlib.pyplot as plt
-#import seaborn as sns
-#sns.set()
 
 
-def fft(array, fs, win='hanning', ylabel_name='Data', xlabel_name='Time'):
-    '''
-    MATLAB関数fftを参考
-    この関数を呼んだあと、必ずplt.show()を叩くこと
+def plotfft(wave, fs, win='hanning', dB=True, vert=[], title='', ylabel='Wave', xlabel='Time [ms]', alpha=0.1):
+    """Plot fft result.
+
+    MATLAB関数fftを参考に実装した。
+    この関数を呼んだあと、必ずplt.show()を叩くこと。
     param
     -----
-    array: array_like
+    wave: array_like
         波形データ
     fs: float
         サンプリング周波数[Hz]
     win: string
-        窓関数指定 デフォルト値は'hanning'
-    ylabel_name: string
-        出力するグラフの縦軸のラベル デフォルト値は'Data'
-    xlabel_name: string
-        出力するグラフの横軸のラベル デフォルト値は'Time'
-    '''
-    # 窓関数で波形を切り出す
-    L = len(array)
+        窓関数指定
+    dB: bool
+        dBへの単位変換 Trueなら単位変換する
+    vert: array_like
+        周波数応答のグラフに目印の縦線を入れる
+    title: string
+        出力するグラフのタイトル
+    ylabel: string
+        出力するグラフの縦軸のラベル
+    xlabel: string
+        出力するグラフの縦軸のラベル
+    alpha: float
+        fftの結果の拡大率
+    """
+    # --- 引数確認 --- #
+    if not (0 < alpha < 1):
+        print('alpha is INVALID value.')
+        return False
+
+    # --- 窓関数で波形を切り出す --- #
+    L = len(wave)
     if win == 'hanning':
         window = np.hanning(L)  # ハン窓
     elif win == 'hamming':
@@ -44,60 +54,85 @@ def fft(array, fs, win='hanning', ylabel_name='Data', xlabel_name='Time'):
     elif win == 'square':
         window = np.ones(L)    # 矩形窓
     else:
-        print('win is fault: ' + win)
-        print('use square window')
+        print(f'{win} is INVALID. Use square window.')
         win = 'square'
         window = np.ones(L)    # 矩形窓
 
-    array_window = array * window
+    # 窓掛け
+    wave_window = wave * window
 
-    # FFT計算
-    # numpy.fftよりscipy.fftpackの方が速い
-    NFFT = 2**nextpow2(L)  # 計算速度向上のため解析データ数に近い2の乗数を計算
-    fft_amp = fftpack.fft(array_window, NFFT)  # 周波数領域のAmplitude
-    fft_fq = fftpack.fftfreq(NFFT, d=1.0/fs)  # Amplitudeに対応する周波数
+    # --- FFT計算 --- #
+    # fftpack->fftに変更
+    NFFT = 2**_nextpow2(L)  # 計算速度向上のため解析データ数に近い2の乗数を計算
+    fft_amp = fft.fft(wave_window, n=NFFT)  # 周波数領域のAmplitude
+    fft_frq = fft.fftfreq(NFFT, d=1.0/fs)  # Amplitudeに対応する周波数
+
     # 正の領域のみ抽出
     fft_amp = fft_amp[0: int(len(fft_amp)/2)]
-    fft_fq = fft_fq[0: int(len(fft_fq)/2)]
-    fft_amp = db(abs(fft_amp))  # 複素数→デシベル変換
+    fft_frq = fft_frq[0: int(len(fft_frq)/2)]
+    if dB:
+        fft_amp = _dB(abs(fft_amp))  # 複素数→デシベル変換
+    else:
+        fft_amp = abs(fft_amp)
 
-    # グラフ表示
-    plt.figure(figsize=(8, 6*1.5))
-    plt.subplots_adjust(hspace=0.4)  # x軸ラベル表示のため余白調整
-    # 入力データプロット
-    plt.subplot(4, 1, 1)
-    plt.plot(array)
-    #plt.xlabel(xlabel_name)
-    plt.ylabel(ylabel_name)
-    plt.grid()
-    # 入力データ*窓関数プロット
-    plt.subplot(4, 1, 2)
-    plt.plot(array_window)
-    plt.xlabel(xlabel_name)
-    plt.ylabel(ylabel_name + '*' + win)
-    plt.grid()
-    # FFTプロット
-    plt.subplot(4, 1, 3)
-    plt.plot(fft_fq, fft_amp)
-    plt.xlim(0, fs/2)
-    #plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dB]')  # |X(omega)|
-    plt.grid()
+    # --- グラフ表示 --- #
+    # fig設定
+    fig = plt.figure(figsize=(8, 6*1.5), tight_layout=True)
+    fig.suptitle(title)
+
+    # timestamp
+    ts = np.array([i for i in range(L)]) * (1000. / fs)
+
+    # 入力データ
+    ax = fig.add_subplot(4, 1, 1)
+    ax.plot(ts, wave)
+    ax.set_ylabel(ylabel)
+    ax.grid(visible=True)
+
+    # 入力データ*窓
+    ax = fig.add_subplot(4, 1, 2)
+    ax.plot(ts, wave_window)
+    ax.set_xlabel('Timestamp [ms]')
+    ax.set_ylabel(ylabel + '*' + win)
+    ax.grid(visible=True)
+
+    # FFT
+    ax = fig.add_subplot(4, 1, 3)
+    ax.plot(fft_frq, fft_amp)
+    ax.set_xlim(0, fs/2)
+    if dB:
+        ax.set_ylabel('Amplitude [dB]')  # |X(omega)|
+    else:
+        ax.set_ylabel('Amplitude')
+    ax.grid(visible=True)
+
     # 低周波領域FFTプロット
-    plt.subplot(4, 1, 4)
-    plt.plot(fft_fq, fft_amp)
-    plt.xlim(0, fs/20)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dB]')  # |X(omega)|
-    plt.grid()
+    ax = fig.add_subplot(4, 1, 4)
+    ax.plot(fft_frq, fft_amp)
+    if vert is not None:
+        for v in vert:
+            ax.plot([v, v], [fft_amp.min(), fft_amp.max()], label=str(v)+'[Hz]')
+        ax.legend()
+    ax.set_xlim(0, fs/2*alpha)
+    ax.set_xlabel('Frequency [Hz]')
+    if dB:
+        ax.set_ylabel('Amplitude [dB]')  # |X(omega)|
+    else:
+        ax.set_ylabel('Amplitude')
+    ax.grid(visible=True)
 
-def nextpow2(n):  # MATLAB関数nextpow2
+    return fig
+
+
+def _nextpow2(n):  # MATLAB関数nextpow2
     m_f = np.log2(n)
     m_i = np.ceil(m_f)
     return int(np.log2(2**m_i))
 
-def fvtool(b, fs, a=1, worN=8192, fc1=None, fc2=None):
-    '''
+
+def plotfreqz(b, fs, a=1, worN=8192, vert=[], title=''):
+    """Plot freqz result.
+
     MATLAB関数fvtoolを参考
     この関数を呼んだあと、必ずplt.show()を叩くこと
     param
@@ -105,53 +140,69 @@ def fvtool(b, fs, a=1, worN=8192, fc1=None, fc2=None):
     b: array_like
         伝達関数の分子の係数
     a: array_like
-        伝達関数の分母の係数 デフォルト値は1
+        伝達関数の分母の係数
     fs: float
         サンプリング周波数[Hz]
     worN: int
         単位円の半円の分割数
         周波数に変換するときは w/pi*fn をする
-    fc1: float
-        周波数応答のグラフにカットオフ周波数などの印（縦線）を入れる
-    fc2: float
-        周波数応答のグラフにカットオフ周波数などの印（縦線）を入れる
-    '''
+    vert: array_like
+        周波数応答のグラフに目印の縦線を入れる
+    title: string
+        出力するグラフのタイトル
+    """
+    # --- F特計算 --- #
     # ナイキスト周波数計算
-    fn = fs / 2
+    fn = fs / 2.0
 
     # 周波数応答計算
     w, h = signal.freqz(b, a, worN)
     x_freq_rspns = w / np.pi * fn
-    y_freq_rspns = db(abs(h))  # 複素数→デシベル変換
+    y_freq_rspns = _dB(abs(h))  # 複素数→デシベル変換
 
     # 群遅延計算
-    w, gd = signal.group_delay([b, a], worN)
+    w, gd = signal.group_delay((b, a), worN)
     x_gd = w / np.pi * fn
     y_gd = gd
 
-    # グラフ表示
-    plt.figure(figsize=(8, 6))
-    # 周波数応答プロット
-    plt.subplot(2, 1, 1)
-    plt.plot(x_freq_rspns, y_freq_rspns)
-    if fc1 is not None:
-        plt.plot([fc1, fc1], [y_freq_rspns.min(), y_freq_rspns.max()])
-    if fc2 is not None:
-        plt.plot([fc2, fc2], [y_freq_rspns.min(), y_freq_rspns.max()])
-    #plt.ylim(-70, 2)  # MATLAB fvtool仕様
-    plt.ylabel('Amplitude [dB]')
-    plt.grid()
-    # 群遅延プロット
-    plt.subplot(2, 1, 2)
-    plt.plot(x_gd, y_gd)
-    plt.ylim(0, len(b))  # MATALB fvtool仕様
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Group delay [samples]')
-    plt.grid()
+    # y_gdの要素にゼロがあったら削除する（SciPyのバグ？）
+    zero_index = np.where(y_gd == 0.0, True, False)
+    if zero_index.sum() > 0:
+        print('output of signal.group_delay has zero value and delete the elements')
+        nonzero_index = np.logical_not(zero_index)
+        x_gd = x_gd[nonzero_index]
+        y_gd = y_gd[nonzero_index]
 
-def db(x, dBref=1):  # デシベル変換
-    y = 20 * np.log10(x / dBref)
+    # --- グラフ表示 --- #
+    fig = plt.figure(figsize=(8, 6), tight_layout=True)
+    fig.suptitle(title)
+
+    # 周波数応答
+    ax = fig.add_subplot(2, 1, 1)
+    ax.plot(x_freq_rspns, y_freq_rspns)
+    if vert is not None:
+        for v in vert:
+            ax.plot([v, v], [y_freq_rspns.min(), y_freq_rspns.max()], label=str(v)+'[Hz]')
+        ax.legend()
+    ax.set_ylabel('Amplitude [dB]')
+    ax.grid(visible=True)
+
+    # 群遅延
+    ax = fig.add_subplot(2, 1, 2)
+    ax.plot(x_gd, y_gd)
+    ax.set_ylim(0, len(b))  # MATLAB fvtool仕様
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_ylabel('Group delay [samples]')
+    ax.grid(visible=True)
+
+    return fig
+
+
+def _dB(x, dBref=1):  # デシベル変換
+    #y = 20 * np.log10(x / dBref)
+    y = 20 * np.log10(x / dBref + 1.0e-06)
     return y
+
 
 def make_MA_filter(N):
     if N % 2 == 0:
